e2f02b4224a0ea42ca89e9fc6727c901e4b73435
anas <anas3ez@gmail.com>
2025-11-28 01:56:42 +0200

    finish task 2

--- tasks1/lib/main.dart
+++ tasks1/lib/main.dart
@@ -1,18 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:tasks/home_screen.dart';
+// import 'package:flutter/material.dart';
+// import 'package:tasks/home_screen.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: HomeScreen(),
+//     );
+//   }
+// }
---
+++ tasks2/lib/task2_screen.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+
+class Task2Screen extends StatelessWidget {
+  const Task2Screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: Appbar(),
+        body: const Padding(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            children: [
+              Center(
+                child: Text(
+                  'Wedensday, 22 May 2019',
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+              ListTile(
+                  title: Text('Task1'),
+                  subtitle: Row(
+                    children: [
+                      Icon(Icons.alarm),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('details for the task')
+                    ],
+                  )),
+              Divider(),
+              ListTile(
+                  title: Text('task 2'),
+                  subtitle: Row(
+                    children: [
+                      Icon(Icons.alarm),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('dadetails for the task')
+                    ],
+                  )),
+              Divider(),
+              ListTile(
+                  title: Text('task 3'),
+                  trailing: Icon(
+                    Icons.close,
+                    color: Colors.red,
+                    size: 32,
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Icon(Icons.alarm),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('dadetails for the task')
+                    ],
+                  )),
+              Divider(),
+              ListTile(
+                  title: Text('task 4'),
+                  trailing: Icon(
+                    Icons.checklist_rtl,
+                    color: Colors.blue,
+                    size: 32,
+                  ),
+                  subtitle: Row(
+                    children: [
+                      Icon(Icons.alarm),
+                      SizedBox(
+                        width: 8,
+                      ),
+                      Text('dadetails for the task')
+                    ],
+                  )),
+              Divider(),
+            ],
+          ),
+        ));
+  }
+
+  AppBar Appbar() {
+    return AppBar(
+      backgroundColor: Colors.blue,
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.menu,
+            color: Colors.white,
+          ),
+        ),
+      ],
+      leading: const Icon(
+        Icons.document_scanner,
+        color: Colors.white,
+      ),
+      title: const Text(
+        'Appointments',
+        style: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
